4ac8ee38c79dee2ef3350691f10afbb91d93c371
mahesh.mummareddy@hotstar.com <mummareddym@Admins-MacBook-Pro-16.local>
2021-03-25 13:48:55 +0530

    pr decorators check

--- src/main/java/com/mahesh/StringHelper.java
+++ src/main/java/com/mahesh/StringHelper.java
@@ -5,6 +5,11 @@ public class StringHelper {
     public static void main(String[] args) {
         System.out.println("testing ....");
         System.out.println(findLength("my length"));
+        System.out.println(findWords("my length"));
+    }
+
+    private static int findWords(String my_length) {
+        return my_length.split(" ").length;
     }
 
     static int findLength(String my_length) {
